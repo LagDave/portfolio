f8b1c822d84c71bdce902e039dbbd584d0b06858
lagdave <laggy80@gmail.com>
2023-03-21 19:33:40 +0800

    component-ize experience item and disabled animation on mobile

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,98 +1,70 @@
 import "./experience.styles.scss";
 
-import GradientHeading from "../GradientHeading/GradientHeading";
+import ExperienceItem from "../ExperienceItem/ExperienceItem";
 
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
-import Icon from "../Icon/Icon";
-import {faBriefcase} from '@fortawesome/free-solid-svg-icons'
-
-import pdfitLogo from "../../images/pdfit-logo.png"
-
 const Experience = () => {
+
+  const isMobile = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  };
+
   return(
     <section id="experience">
       <div className="container">
       <h2 data-aos="fade-right" className="polysans-bulky-wide text-white">Experience<span className="sub text-green barlow-condensed-light"> — OH, I HAVE A LOT OF STORIES TO TELL!</span></h2>
-
         <div className="vertical-timeline-wrapper">
 
-          <VerticalTimeline>
+          <VerticalTimeline
+            animate={!isMobile()}
+          >
 
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              contentArrowStyle={{ borderRight: '7px solid  #fff' }}
-              icon={<Icon icon={faBriefcase}/>}
-            >
+            <ExperienceItem
+              title="Full-stack Web Developer"
+              subtitle='FGS Advisory — Sydney, Australia'
+              details={
+                [
+                  'Designed and Developed internal web applications and software products for the company.'
+                ]
+              }
+            />
 
-            <h3 className="vertical-timeline-element-title polysans-neutral">Full-stack Web Developer</h3>
-            <h6 className="vertical-timeline-element-subtitle polysans-neutral mt-2">FGS Advisory — Sydney, Australia</h6>
-            <ul>
-              <li>
-                <p> Designed and Developed internal web applications and software products for the company. </p>
-              </li>
-            </ul>
-            
-            </VerticalTimelineElement>
+            <ExperienceItem
+              title="Web Development Consultant"
+              subtitle='wesmcdowell.com — Chicago, USA'
+              details={
+                [
+                  'Created custom web solutions for technical business problems',
+                  'Developed WordPress websites for different requirements'
+                ]
+              }
+            />
 
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              contentArrowStyle={{ borderRight: '7px solid  #fff' }}
-              icon={<Icon icon={faBriefcase}/>}
-            >
+            <ExperienceItem
+              title="Web Development Consultant"
+              subtitle='2fsolutions.com — Italy'
+              details={
+                [
+                  'Designed and developed websites for agency clients with Wix, Shopify, WordPresss and custom scripts.',
+                  'Troubleshot, optimized, and formulated solutions for faulty, existing websites.'
+                ]
+              }
+            />
 
-            <h3 className="vertical-timeline-element-title polysans-neutral">Web Development Consultant</h3>
-            <h6 className="vertical-timeline-element-subtitle polysans-neutral mt-2">wesmcdowell.com — Chicago, USA</h6>
-            <ul>
-              <li>
-                <p className="text-slate">Created custom web solutions for technical business problems</p>
-              </li>
-              <li>
-              <p className="text-slate">Developed WordPress websites for different requirements</p>
-              </li>
-            </ul>
-
-            </VerticalTimelineElement>
-
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              contentArrowStyle={{ borderRight: '7px solid  #fff' }}
-              icon={<Icon icon={faBriefcase}/>}
-            >
-
-            <h3 className="vertical-timeline-element-title polysans-neutral">Web Development Consultant</h3>
-            <h6 className="vertical-timeline-element-subtitle polysans-neutral mt-2">2fsolutions.com — Italy</h6>
-            <ul>
-              <li>
-                <p className="text-slate">Designed and developed websites for agency clients with Wix, Shopify, WordPresss and custom scripts.</p>
-              </li>
-              <li>
-              <p className="text-slate">Troubleshot, optimized, and formulated solutions for faulty, existing websites.</p>
-              </li>
-            </ul>
-            </VerticalTimelineElement>
-
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              contentArrowStyle={{ borderRight: '7px solid  #fff' }}
-              icon={<Icon icon={faBriefcase}/>}
-            >
-
-            <h3 className="vertical-timeline-element-title polysans-neutral">WordPress Developer</h3>
-            <h6 className="vertical-timeline-element-subtitle polysans-neutral mt-2">Web Propellers — Davao, Philippines</h6>
-            <ul>
-              <li>
-                <p className="text-slate">Developed WordPress websites for an array of industries </p>
-              </li>
-              <li>
-              <p className="text-slate">Engineered web solutions to accommodate brilliant business ideas Provided outstanding technical support for english, and non-english clients</p>
-              </li>
-            </ul>
-
-            </VerticalTimelineElement>
-
-
+            <ExperienceItem
+              title="WordPress Developer"
+              subtitle='Web Propellers — Davao, Philippines'
+              details={
+                [
+                  'Developed WordPress websites for an array of industries',
+                  'Engineered web solutions to accommodate brilliant business ideas Provided outstanding technical support for english, and non-english clients'
+                ]
+              }
+            />
           </VerticalTimeline>
 
         </div>
