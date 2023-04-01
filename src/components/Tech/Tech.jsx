884697d0bff1472928d5c0e6f6cf618e74a168d0
lagdave <laggy80@gmail.com>
2023-04-01 23:15:10 +0800

    improved styles; added contact section

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -71,16 +71,11 @@ const Tech = () => {
       detectRetina: true,
   }
 
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
+    const particlesInit = useCallback(async engine => {
       await loadFull(engine);
     }, []);
 
     const particlesLoaded = useCallback(async container => {
-        await console.log(container);
     }, []);
 
   return (
