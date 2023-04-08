c7dceaad75ff84ee451486e0afe21dbf9cdc2d3b
lagdave <laggy80@gmail.com>
2023-04-08 14:50:41 +0800

    removed ts particles as it is slow

--- src/components/TechCard/TechCard.jsx
+++ src/components/TechCard/TechCard.jsx
@@ -5,12 +5,8 @@ import Tilt from 'react-parallax-tilt';
 const TechCard = ({isImage = false, imageURL, deviconClasses, title, whiteCardBG = false}) => {
   return ( 
 
-    <Tilt
-        className="parallax-effect-glare-scale tech-card"
-        tiltMaxAngleX={25}
-        tiltMaxAngleY={25}
-        perspective={800}
-        transitionSpeed={100}
+    <div
+        className="tech-card"
     >
         <div className="inner-element">
             {
@@ -20,10 +16,9 @@ const TechCard = ({isImage = false, imageURL, deviconClasses, title, whiteCardBG
                 <i className={`devicon ${deviconClasses}`}></i>
               )
             }
-            
             <p className="text-center my-2 space-mono">{title}</p>
         </div>
-    </Tilt>
+    </div>
 
    );
 }
